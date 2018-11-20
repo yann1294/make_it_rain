a66715fc25b4a64bee1f3649acbedb60e102379e
A. Abraham Yann <dev.yann12@gmail.com>
2018-11-21 01:02:55 +0530

    Initial commit

---
+++ lib/UI/MakeItRain.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MakeItRain extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MakeItRainState();
+  }
+}
+
+class MakeItRainState extends State<MakeItRain> {
+
+  int _moneyCounter= 0;
+
+  void _rainMoney(){
+    // Important - setState is called each time we need to update the UI
+    setState(() {
+      _moneyCounter = _moneyCounter + 100;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Make It Rain!"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            //title
+            new Center(
+             child: new Text(
+                "Get Rich!",
+                style: new TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 29.9)
+              )
+            ),
+            new Expanded(
+                child: new Center(
+                  child: new Text(
+                    '\$$_moneyCounter',
+                    style: new TextStyle(
+                      color: _moneyCounter > 2000 ? Colors.blueAccent : Colors.red,
+                      fontSize: 46.9,
+                      fontWeight: FontWeight.w800
+                    ),
+                  ),
+            )),
+            new Expanded(
+                child: new Center(
+                  child: new FlatButton(
+                    color: Colors.lightGreen,
+                      textColor: Colors.white70,
+                      onPressed: _rainMoney,
+                      child: new Text(
+                        "Let It Rain",
+                        style: new TextStyle(
+                          fontSize: 19.9,
+
+                        ),
+                      )
+                  ),
+                )
+
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:make_it_rain/UI/MakeItRain.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: "Make it Rain",
+    home: new MakeItRain(),
+  ));
+}
